bdffca51007e2ee467cca4ed5a1ef1d328b444ef
Aleksei <98910189+09121988stroitel1@users.noreply.github.com>
2022-12-01 11:59:13 +0300

    refactor: new selector

--- src/redux/users-selectors.js
+++ src/redux/users-selectors.js
@@ -6,3 +6,19 @@ export const getPageSize = (state) => {
   return state.usersPage.PageSize
 }
   
+export const getTotalUsersCount = (state) => {
+  return state.usersPage.totalUsersCoun
+}
+
+export const getCurrentPage = (state) => {
+  return state.usersPage.currentPage
+}
+
+export const getIsFetching = (state) => {
+  return state.usersPage.isFetching
+}
+
+export const getFollowingInProgress = (state) => {
+  return state.usersPage.followingInProgress
+}
+
